3c074d2e04e0bfede41317717038dfc10bde6c3a
Lucas Roberto <33943648+lrobertodaldegan@users.noreply.github.com>
2019-05-15 10:55:16 -0300

    Add files via upload

--- db/ddl/createTableCompartilhaContaUsuario.sql
+++ db/ddl/createTableCompartilhaContaUsuario.sql
@@ -1,8 +1,10 @@
 CREATE TABLE meubolso.compartilha_conta_usuario (
     id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     id_conta INT(10) NOT NULL,
-    id_usuario INT(10) NOT NULL
+    id_usuario INT(10) NOT NULL,
+    id_owner INT(10) NOT NULL
 );
 
-ALTER TABLE meubolso.rl_conta_usuario ADD FOREIGN KEY (id_usuario) REFERENCES meubolso.usuario(id);
-ALTER TABLE meubolso.rl_conta_usuario ADD FOREIGN KEY (id_conta) REFERENCES meubolso.conta(id);
+ALTER TABLE meubolso.compartilha_conta_usuario ADD FOREIGN KEY (id_usuario) REFERENCES meubolso.usuario(id);
+ALTER TABLE meubolso.compartilha_conta_usuario ADD FOREIGN KEY (id_owner) REFERENCES meubolso.usuario(id);
+ALTER TABLE meubolso.compartilha_conta_usuario ADD FOREIGN KEY (id_conta) REFERENCES meubolso.conta(id);
